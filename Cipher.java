9f6dbe09339ab98fd83709089c335830cef4f1de
Geethika559 <141471620+Geethika559@users.noreply.github.com>
2023-12-29 12:09:09 +0530

    Add files via upload

--- Cipher.java
+++ Cipher.java
@@ -1,26 +1,58 @@
-import java.io.*;
-import java.util.*;
-public class Cipher{
-public static String encrypt(String message,int shift){
-  message=message.toUpperCase();
-  String cipherText=" ";
-  for(int i=0;i<message.length();i++){
-    char ch=(char)(((int)message.charAt(i)+shift-65)%26+65);
-    cipherText=cipherText+ch;
-  }
-  return cipherText;  
-}
-public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);  
-    Random random=new Random();
-    int min=1;
-    int max=25;
-    int shift=random.nextInt(max-min+1)+min;
-    System.out.println("Enter the string");
-    String message=sc.next();
-    System.out.println("String: "+message);
-    System.out.println("Shift: "+shift);
-    System.out.println("Cipher:"+encrypt(message,shift));
+import java.util.Scanner;
+
+public class Cipher {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the text to encrypt: ");
+        String plainText = scanner.nextLine();
+        System.out.print("Enter the secret key: ");
+        int key = scanner.nextInt();
+        String caesarCipherText = caesarCipherEncrypt(plainText, key);
+        System.out.println("Cipher Text: " + caesarCipherText);
+        String caesarDecryptedText = caesarCipherDecrypt(caesarCipherText, key);
+        System.out.println("Caesar Cipher Decrypted Text: " + caesarDecryptedText);
+        cryptanalysis(plainText, caesarCipherText);
+
+        scanner.close();
     }
-  }
-  
+
+    private static String caesarCipherEncrypt(String text, int key) {
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (char ch : text.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                char base = Character.isUpperCase(ch) ? 'A' : 'a';
+                encryptedText.append((char) ((ch - base + key) % 26 + base));
+            } else {
+                encryptedText.append(ch);
+            }
+        }
+
+        return encryptedText.toString();
+    }
+
+    private static String caesarCipherDecrypt(String text, int key) {
+        return caesarCipherEncrypt(text, 26 - key);
+    }
+
+    private static void cryptanalysis(String original, String caesarCipher) {
+        System.out.println("\nCryptanalysis Results:");
+        int shift=0;
+
+        System.out.println("Original Text: " + original);
+
+        System.out.println("Caesar Cipher Decryption Attempts:");
+        for (int i = 1; i <= 26; i++) {
+            String decryptedText = caesarCipherDecrypt(caesarCipher, i);
+            System.out.println("Key " + i + ": " + decryptedText);  
+            if (decryptedText.equalsIgnoreCase(original)) {
+              shift=i;
+            }
+        }
+        System.out.println("Match found at key :"+shift);
+    }
+}
+
+
+
